9cc6bbfa6af48e37cc1d3ed90b0c650fa87c4378
soldierofhell <45559276+soldierofhell@users.noreply.github.com>
2020-03-17 12:41:15 +0100

    Update Trainer.cpp

--- Trainer.cpp
+++ Trainer.cpp
@@ -4,7 +4,7 @@
 #include "Trainer.h"
 #include "dqn.h"
 #include "ExperienceReplay.h"
-#include "/Users/navneetmadhukumar/Downloads/Arcade-Learning-Environment-master/src/ale_interface.hpp"
+#include "/home/miron/rl/dustynv_rl/Arcade-Learning-Environment/src/ale_interface.hpp"
 #include <math.h>
 #include <chrono>
 
